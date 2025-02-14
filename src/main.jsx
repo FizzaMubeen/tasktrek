1a2c3ada2d83cad32c8679e0a5bee640c547afe2
Fizza Mubeen <mubeenfizza@gmail.com>
2025-02-14 20:01:06 +0500

    Add new features

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 //import App from "./App.jsx";
-import Card from "./components/Card.jsx";
-import CreateToDo from "./components/CreateToDo.jsx";
+
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Card />
-    <CreateToDo />
+    <App />
   </StrictMode>
 );
 
