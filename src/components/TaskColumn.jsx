6894f851825b78153dad54df54fd6b24ccc3230e
Fizza Mubeen <mubeenfizza@gmail.com>
2025-02-16 22:26:22 +0500

    Progress 16-02-2025

--- src/components/TaskColumn.jsx
+++ src/components/TaskColumn.jsx
@@ -3,15 +3,18 @@ import Todo from "../assets/direct-hit.png";
 import Doing from "../assets/glowing-star.png";
 import Done from "../assets/check-mark-button.png";
 import "./TaskColumn.css";
+import TaskCard from "./TaskCard";
 
 const TaskColumn = (props) => {
+  const { title, icon } = props;
   return (
     <div className="task_column">
       <section className="task_column">
         <h2 className="task_column_heading">
-          <img className="task_column_icon" src={props.icon} alt="" />{" "}
-          {props.title}
+          <img className="task_column_icon" src={icon} alt="" /> {title}
         </h2>
+
+        <TaskCard />
       </section>
 
       {/* <section className="task_column">
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,16 +1,49 @@
 //rafce
-import React from "react";
+import React, { useState } from "react";
 import "./TaskForm.css";
 import Tag from "./Tag";
 
 const TaskForm = () => {
+  const [taskData, settaskData] = useState({
+    task: "",
+    status: "To Do",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target; //same as below
+    //const name = e.target.name;
+    //const value = e.target.value;
+
+    settaskData((prev) => {
+      return { ...prev, [name]: value };
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(taskData);
+  };
+
+  // const [task, setTask] = useState("");
+  // const [status, setStatus] = useState("To Do");
+  // const handleTaskChange = (e) => {
+  //   setTask(e.target.value);
+  // };
+
+  // const handleStatusChange = (e) => {
+  //   setStatus(e.target.value);
+  // };
+
+  // console.log(task, status);
   return (
     <header className="app_header">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="text"
+          name="task"
           className="task_input"
           placeholder="Enter Your Task"
+          onChange={handleChange}
         />
         <div className="task_form_bottom_line">
           <div>
@@ -25,7 +58,11 @@ const TaskForm = () => {
             <button className="tag">React</button> */}
           </div>
           <div>
-            <select className="task_status">
+            <select
+              name="status"
+              className="task_status"
+              onChange={handleChange}
+            >
               <option value="todo">To Do</option>
               <option value="doing">Doing</option>
               <option value="done">Done</option>
